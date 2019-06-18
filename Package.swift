446fea6fdf0421b7cbfaf433658b897704647b4b
Martin Pucik <martin.pucik@me.com>
2019-06-18 19:22:03 +0200

    setup

--- Package.swift
+++ Package.swift
@@ -6,23 +6,11 @@ import PackageDescription
 let package = Package(
     name: "MPUtils",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "MPUtils",
-            targets: ["MPUtils"]),
+        .library(name: "MPUtils", targets: ["MPUtils"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "MPUtils",
-            dependencies: []),
-        .testTarget(
-            name: "MPUtilsTests",
-            dependencies: ["MPUtils"]),
+        .target(name: "MPUtils", dependencies: []),
+        .testTarget(name: "MPUtilsTests", dependencies: ["MPUtils"]),
     ]
 )
---
+++ Sources/MPUtils/Views/MPUInputAccessoryKeyboardView.swift
@@ -0,0 +1,60 @@
+//
+//  MPUInputAccessoryKeyboardView.swift
+//  MPUtils
+//
+//  Created by Martin Pucik on 23/08/2018.
+//
+
+import UIKit
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// MARK: - CDCInputAccessoryKeyboardViewProtocol
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+public protocol MPUInputAccessoryKeyboardViewProtocol: class {
+    func keyboardFrameChanged(_ frame: CGRect)
+}
+
+public class MPUInputAccessoryKeyboardView: UIView {
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // MARK: - Private Properties
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private weak var delegate: MPUInputAccessoryKeyboardViewProtocol?
+    private var centerObserver: NSKeyValueObservation?
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // MARK: - Lifecycle
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public init(delegate: MPUInputAccessoryKeyboardViewProtocol) {
+        self.delegate = delegate
+        super.init(frame: CGRect.zero)
+        autoresizingMask = .flexibleHeight
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        return nil
+    }
+
+    deinit {
+        centerObserver?.invalidate()
+        centerObserver = nil
+    }
+
+    override public var intrinsicContentSize: CGSize {
+        return CGSize.zero
+    }
+
+    override public func willMove(toSuperview newSuperview: UIView?) {
+        if newSuperview == nil {
+            centerObserver?.invalidate()
+            centerObserver = nil
+        } else {
+            centerObserver = newSuperview?.observe(\.center, options: [.new], changeHandler: { [weak self] (view, _) in
+                self?.delegate?.keyboardFrameChanged(view.frame)
+            })
+        }
+    }
+}
