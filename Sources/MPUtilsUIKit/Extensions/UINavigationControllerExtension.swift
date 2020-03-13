f30b140e15a8e436d2843e22a3408a87c2cba6ac
martinpucik <martin.pucik@me.com>
2020-03-13 19:40:32 +0100

    UINavigationController method availability
    tvOS unavailable

--- Sources/MPUtilsUIKit/Extensions/UINavigationControllerExtension.swift
+++ Sources/MPUtilsUIKit/Extensions/UINavigationControllerExtension.swift
@@ -13,6 +13,7 @@ public extension UINavigationController {
     ///   - image: optional UIImage to be used as UINavigationBar.backIndicatorImage
     ///   - tintColor: optional tint color to use as backBarButtonItem.tintColor
     @available(iOS 7.0, *)
+    @available(tvOS, unavailable)
     func makeEmptyBack(withImage image: UIImage? = nil, tintColor: UIColor? = nil) {
         navigationBar.backIndicatorImage = image?.withRenderingMode(.alwaysTemplate)
         navigationBar.backIndicatorTransitionMaskImage = image?.withRenderingMode(.alwaysTemplate)
