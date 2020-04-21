83350e6d489149a9132900330bc30d3d449ce6a2
martinpucik <martin.pucik@me.com>
2020-04-21 18:58:13 +0200

    Added register/dequeue for collection header views

--- Sources/MPUtilsUIKit/Extensions/UICollectionViewExtension.swift
+++ Sources/MPUtilsUIKit/Extensions/UICollectionViewExtension.swift
@@ -19,8 +19,7 @@ public extension UICollectionView {
         let reuseIdentifier = suffix.isEmpty ? cellClass.identifier : "\(cellClass.identifier)_\(suffix)"
         register(cellClass, forCellWithReuseIdentifier: reuseIdentifier)
     }
-
-    /// Dequeues cell from table view with set type
+    /// Dequeues cell from collection view with set type
     func dequeueReusableCell<Cell: UICollectionViewCell>(forIndexPath indexPath: IndexPath, identifierSuffix: String? = nil) -> Cell {
         let suffix = identifierSuffix ?? ""
         let reuseIdentifier = suffix.isEmpty ? Cell.identifier : "\(Cell.identifier)_\(suffix)"
@@ -28,6 +27,27 @@ public extension UICollectionView {
             fatalError("CollectionView Cell at indexPath \(indexPath) is not expected type")
         }
         return cell
+    }
+    /// Registers header view class just with the class type
+    /// - Parameters:
+    ///   - headerClass: Header class type
+    ///   - identifierSuffix: optional suffix to use in reuse identifier
+    func registerHeader<Header: UICollectionReusableView>(_ headerClass: Header.Type, identifierSuffix: String? = nil) {
+        let suffix = identifierSuffix ?? ""
+        let reuseIdentifier = suffix.isEmpty ? headerClass.identifier : "\(headerClass.identifier)_\(suffix)"
+        register(headerClass, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: reuseIdentifier)
+    }
+    /// Dequeues header from collection view with set type
+    func dequeueReusableHeader<Header: UICollectionReusableView>(forIndexPath indexPath: IndexPath, identifierSuffix: String? = nil) -> Header {
+        let suffix = identifierSuffix ?? ""
+        let reuseIdentifier = suffix.isEmpty ? Header.identifier : "\(Header.identifier)_\(suffix)"
+        guard let header = dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader,
+                                                            withReuseIdentifier: reuseIdentifier,
+                                                            for: indexPath) as? Header
+        else {
+            fatalError("CollectionView Header at indexPath \(indexPath) is not expected type")
+        }
+        return header
     }
 }
 #endif
