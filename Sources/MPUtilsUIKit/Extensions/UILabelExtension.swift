600c2d2e16bf54a4756cfc3a745e5f6be86e89c9
martinpucik <martin.pucik@me.com>
2020-05-02 14:35:38 +0200

    Added UILabel extension

--- Sources/MPUtilsUIKit/Extensions/UILabelExtension.swift
+++ Sources/MPUtilsUIKit/Extensions/UILabelExtension.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import UIKit
+
+public extension UILabel {
+    /// Creates UILabel instance set up with style from passed arguments
+    /// - Parameters:
+    ///   - text: text to be set in the label
+    ///   - font: The font used to display the text.
+    ///   - textColor: The color of the text.
+    ///   - alignment: The technique to use for aligning the text.
+    convenience init(text: String? = nil, font: UIFont? = nil, textColor: UIColor? = nil, alignment: NSTextAlignment? = nil) {
+        self.init()
+        self.text = text
+        self.font = font ?? self.font
+        self.textColor = textColor ?? self.textColor
+        self.textAlignment = alignment ?? self.textAlignment
+    }
+}
