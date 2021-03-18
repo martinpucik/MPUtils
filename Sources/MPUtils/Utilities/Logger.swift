67f62d2d9923d3ca1b33d41982bc7f49c0cc0d46
martinpucik <martin.pucik@me.com>
2021-03-18 17:09:39 +0100

    Add formatted time to logger

--- Sources/MPUtils/Utilities/Logger.swift
+++ Sources/MPUtils/Utilities/Logger.swift
@@ -12,21 +12,25 @@ import Foundation
 // MARK: - Log methods
 
 public func INFO(_ message: Any?, file: StaticString = #file, line: UInt = #line, funcion: StaticString = #function) {
-    print("💙 [\(lastFilePath(from: file)):\(line) - \(funcion)] \(message ?? "")")
+    print("\(formattedDate) | 💙 [\(lastFilePath(from: file)):\(line) - \(funcion)] \(message ?? "")")
 }
 
 public func WARN(_ message: Any?, file: StaticString = #file, line: UInt = #line, funcion: StaticString = #function) {
-    print("💛 [\(lastFilePath(from: file)):\(line) - \(funcion)] \(message ?? "")")
+    print("\(formattedDate) | 💛 [\(lastFilePath(from: file)):\(line) - \(funcion)] \(message ?? "")")
 }
 
 public func ERROR(_ message: Any?, file: StaticString = #file, line: UInt = #line, funcion: StaticString = #function) {
-    print("❌ [\(lastFilePath(from: file)):\(line) - \(funcion)] \(message ?? "")")
+    print("\(formattedDate) | ❌ [\(lastFilePath(from: file)):\(line) - \(funcion)] \(message ?? "")")
 }
 
 // MARK: - Private Methods
 
 private func lastFilePath(from string: StaticString) -> String {
     return ("\(string)" as NSString).lastPathComponent
+}
+
+private var formattedDate: String {
+    return ISO8601DateFormatter().string(from: Date())
 }
 
 #endif
